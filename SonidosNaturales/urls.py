db34fbb3251b0f318c0b6ca2cbc839e59bdc8515
NASHELY MIKAELA BETANCOURT TOSCANO <nashely.betancourt6156@utc.edu.ec>
2025-07-21 22:28:09 -0500

    Templates de Ubicaciones'

--- SonidosNaturales/urls.py
+++ SonidosNaturales/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('animal/', include('Aplicaciones.Animal.urls')),
     path('ubicacion/', include('Aplicaciones.Ubicacion.urls')),
-    path('grabacion/', include('Aplicaciones.Grabacion.urls')),
+    
 ]
