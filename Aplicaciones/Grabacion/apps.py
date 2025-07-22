b6f1993504551a6749f29a1b2d52ec7ca9ccca5c
NASHELY MIKAELA BETANCOURT TOSCANO <nashely.betancourt6156@utc.edu.ec>
2025-07-21 19:27:26 -0500

    apps.py de las Aplicaciones

--- Aplicaciones/Grabacion/apps.py
+++ Aplicaciones/Grabacion/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class GrabacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Grabacion'
+    name = 'Aplicaciones.Grabacion'
--- Aplicaciones/Ubicacion/apps.py
+++ Aplicaciones/Ubicacion/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class UbicacionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Ubicacion'
+    name = 'Aplicaciones.Ubicacion'
