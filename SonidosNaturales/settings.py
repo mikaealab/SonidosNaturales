3fee4db8a4fd04700a255a6941903893a7e06ef8
NASHELY MIKAELA BETANCOURT TOSCANO <nashely.betancourt6156@utc.edu.ec>
2025-07-21 19:49:59 -0500

    Base de datos con PostgreSQL

--- SonidosNaturales/settings.py
+++ SonidosNaturales/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'SonidosNaturales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sonidos_naturales',
+        'USER': 'usuario_sonidos',
+        'PASSWORD': 'mika2002',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
