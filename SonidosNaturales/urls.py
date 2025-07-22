0b7d100e69f945a12ce7c8da1f46be560d2b1841
NASHELY MIKAELA BETANCOURT TOSCANO <nashely.betancourt6156@utc.edu.ec>
2025-07-22 02:51:01 -0500

    cambios en animales

--- SonidosNaturales/urls.py
+++ SonidosNaturales/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from django.conf import settings
-from django.conf.urls.static import static
-from Aplicaciones.Animal.views import home  # <--- importa la vista home
+from django.conf.urls.static import static  # ✅ IMPORTANTE
+from Aplicaciones.Animal.views import home 
 
 urlpatterns = [
-    path('', home, name='inicio'),  # ahora muestra home.html en la raíz
+    path('', home, name='inicio'),
     path('admin/', admin.site.urls),
-
     path('animal/', include('Aplicaciones.Animal.urls')),
     path('ubicacion/', include('Aplicaciones.Ubicacion.urls')),
     path('grabacion/', include('Aplicaciones.Grabacion.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# ✅ ESTA PARTE ES CRUCIAL PARA SERVIR /media/ EN MODO DEBUG
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
